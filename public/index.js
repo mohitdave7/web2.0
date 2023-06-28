931fd3fff3f9953a72c6bc516aecd3d7451e5e27
mohitdave0@gmail.com <mohitdave0@gmail.com>
2023-06-28 13:34:34 +0530

    added paypal integration and session for storage

--- public/index.js
+++ public/index.js
@@ -1,45 +1,101 @@
+const logo1 = './images/first.png';
+const logo2 = './images/second.jpeg';
+const logo3 = './images/third.jpeg';
+const logo4 = './images/fourth.jpeg';
+
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       amount: 1000,
-      total_amount : 2000,
-      email:'a@gmail.com'
-    }
+      total_amount: 2000,
+      email: 'a@gmail.com',
+      error: null
+    };
   }
+
   async componentDidMount() {
     const result = await axios.get('/get_info');
-    console.log(result)
-    this.setState({total_amount:result.data[0]?.total_amount})
+    console.log(result);
+    this.setState({ total_amount: result.data[0]?.total_amount });
   }
-  onSubmit=async(e)=>{
+
+  onSubmit = async (e) => {
     e.preventDefault();
-    const reponse = await axios.post('/post_info',{
-      amount : this.state.amount,
-      email : this.state.email
+    console.log("inclick");
+
+    if (this.state.amount <= 1) {
+      this.setState({ error: 'Amount must be greater than one' });
+      return;
+    }
+
+    const response = await axios.post('/post_info', {
+      amount: this.state.amount,
+      email: this.state.email
     });
-    console.log(reponse);
+    console.log('.....', response);
+    console.log("response", response.data);
+    window.location.href = response.data;
   }
+
   render() {
     return (
-      <div>
-        <h1>LOTTERY APPLICATION WEB 2.0</h1>
-        <div><p>Total lottery amount is {this.state.total_amount}</p></div>
-        <div>
-          <form onSubmit={this.onSubmit}>
-            <div>
-              <input placeholder="amount" value={this.state.amount} onChange={(e)=>this.setState({amount:e.target.value})}/>
+      <div className="container">
+        <div className="logo-container">
+          <img src={logo1} alt="Logo" className="logo" />
+        </div>
+        <h1 className="lottery-font">LOTTERY APPLICATION WEB 2.0</h1>
+        <div className="marquee-container">
+          <div className="marquee">
+            <p className="marquee-text">Welcome to the Lottery Application!</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title text-center">Total Winning Pool</h5>
+            <div className="d-flex justify-content-center align-items-center bg-primary rounded-circle mx-auto w-50 h-50 mt-3">
+              <p className="text-white fs-3 fw-bold m-0">${this.state.total_amount}</p>
             </div>
-            <div>
-                <input placeholder="email" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})}/>
-            </div>   
-            <div> <button type='submit'>participate</button></div>
-    
-          </form>
+            <form onSubmit={this.onSubmit}>
+              <div className="mb-3 mt-4">
+                <input
+                  className="form-control"
+                  type="number"
+                  min="1"
+                  placeholder="Enter Amount"
+                  value={this.state.amount}
+                  onChange={(e) => this.setState({ amount: e.target.value, error: null })}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  className="form-control"
+                  type="email"
+                  placeholder="Enter Email"
+                  value={this.state.email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                  required
+                />
+              </div>
+              {this.state.error && (
+                <div className="alert alert-danger">{this.state.error}</div>
+              )}
+              <div className="text-center">
+                <button className="btn btn-primary" type="submit">Participate</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="lottery-logos">
+          <img src={logo2} alt="Logo 2" className="logo" />
+          <img src={logo3} alt="Logo 3" className="logo" />
+          <img src={logo4} alt="Logo 4" className="logo" />
         </div>
       </div>
     );
   }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('reactBinding'));
--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const {save_user_information, get_total_amount} = require('./models/server_db');
 const path  = require('path');
 const publicPath = path.join(__dirname, './public');
 var paypal = require('paypal-rest-sdk');
+const session = require('express-session');
+
+
+
+app.use(session({
+    secret: 'my web app',
+    cookie:{maxAge : 60000}
+}))
+
+
+
 //handling all the parsing
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
@@ -15,28 +26,99 @@ paypal.configure({
     'client_id': 'AcKiGWqOXoN9WbJc6dOe47u6yMeyvEvStNpzoveACYrahKpmUY9kKwEkuGdurYI-nhiWMhLY3WFLir9N',
     'client_secret': 'EELx-xltTaDGuM0I7G0TeE7_z519kyDy5ASMduOC2TuWD1xr84GVh5-vYu5ATWNBqeK9fCZESP0Tf0jD'
   });
-app.post('/post_info', async(req,res)=>{
+
+app.post('/post_info', async (req, res) => {
     var email = req.body.email;
     var amount = req.body.amount;
-    if(amount <= 1){
-        return_info ={};
-        return_info.error = true;
-        return_info.message = 'amount must be greater than one';
-        return res.send(return_info);
+    if (amount <= 1) {
+      return res.send({ error: true, message: 'amount must be greater than one' });
     }
-  try {
-    const result = await save_user_information({ "amount": amount, "email": email });
-    res.send(result);
-  } catch (error) {
-    console.error("Error saving user information:", error.message);
-    res.status(500).send("Error saving user information");
-  }
-})
+  
+    try {
+        var fee_amount = amount * 0.9;
+      const result = await save_user_information({ amount: amount, email: email });
+        req.session.paypal_amount = amount;
+      var create_payment_json = {
+                    "intent": "sale",
+                    "payer": {
+                        "payment_method": "paypal"
+                    },
+                    "redirect_urls": {
+                        "return_url": "http://localhost:5000/success",
+                        "cancel_url": "http://localhost:5000/cancel"
+                    },
+                    "transactions": [{
+                        "item_list": {
+                            "items": [{
+                                "name": "Lottery",
+                                "sku": "Funding",
+                                "price": amount,
+                                "currency": "USD",
+                                "quantity": 1
+                            }]
+                        },
+                        "amount": {
+                            "currency": "USD",
+                            "total": amount,
+                        },
+                        "payee" : { "email": "sb-y5twi26188419@business.example.com"},
+                        "description": "Lottery Purchase."
+                    }]
+                };
+      paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+          throw error;
+        } else {
+          let approvalUrl;
+          for (var i = 0; i < payment.links.length; i++) {
+            if (payment.links[i].rel == 'approval_url') {
+              approvalUrl = payment.links[i].href;
+              break;
+            }
+          }
+          if (approvalUrl) {
+            return res.send(approvalUrl);
+          } else {
+            console.log("Approval URL not found.");
+            return res.status(500).send("Error creating PayPal payment");
+          }
+        }
+      });
+    } catch (error) {
+      console.error("Error saving user information:", error.message);
+      res.status(500).send("Error saving user information");
+    }
+  });
+
+  app.get('/success',async (req,res)=>{
+    const payerId =req.query.PayerID;
+    const paymentId =req.query.paymentId;
+    var execute_payment_json ={
+        "payer_id": payerId,
+        "transactions": [{
+            amount:{
+                "currency" : "USD",
+                total : req.session.paypal_amount
+            }
+        }],
+    }
+    paypal.payment.execute(paymentId,execute_payment_json,function(err,payment){
+        if(err){
+            console.log(err.response);
+            throw err;
+        }else{
+            console.log(payment);
+        }
+
+    })
+    res.redirect('http://localhost:5000')
+
+  })
+  
 app.get('/get_info', async (req,res)=>{
     try{
 
         var result = await get_total_amount();
-        console.log(result);
         res.send(result)
     }catch(error){
         console.error("Error getting total amount:", error.message);
@@ -44,6 +126,8 @@ app.get('/get_info', async (req,res)=>{
       
     }
 });
+
+
 
 
 
